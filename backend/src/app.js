b25b56753a70f4aea807b4b190396d65dba460e2
amit-prajapati-ap <prajapatiapamit@gmail.com>
2025-06-19 18:41:00 +0530

    fix: Update allowedOrigins to use environment variable for CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,11 +5,11 @@ import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
 
 const app = express()
-const allowedOrigins = ['http://localhost:5173', '*']
+const allowedOrigins = process.env.ALLOWED_ORIGINS
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({credentials: true, origin: allowedOrigins}))
+app.use(cors({credentials: true, origin: allowedOrigins.split(",")}))
 
 app.get('/', (_,res) => {
     res.send("API Working")
